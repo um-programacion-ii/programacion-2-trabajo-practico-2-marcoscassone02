c94e936812df552a529e0e94ce6e024f892b3b8d
marcoscassone02 <ma.cassone@alumno.um.edu.ar>
2025-04-22 16:29:23 -0300

    Manejo de excepciones

--- src/app/modelo/gestor/GestorUsuarios.java
+++ src/app/modelo/gestor/GestorUsuarios.java
@@ -1,23 +1,36 @@
 package programacion_2_trabajo_practico_2_marcoscassone02.src.app.modelo.gestor;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import programacion_2_trabajo_practico_2_marcoscassone02.src.app.modelo.recurso.excepcion.UsuarioNoEncontradoException;
 import programacion_2_trabajo_practico_2_marcoscassone02.src.app.modelo.usuario.Usuario;
 
 public class GestorUsuarios {
-    private Map<String, Usuario> usuarios = new HashMap<>();
+    private Map<String, Usuario> usuariosPorEmail = new HashMap<>();
+    private List<Usuario> usuarios = new ArrayList<>();
 
     public void agregarUsuario(Usuario usuario) {
-        usuarios.put(usuario.getEmail(), usuario);
+        usuariosPorEmail.put(usuario.getEmail(), usuario);
+        usuarios.add(usuario);
     }
 
     public Usuario obtenerUsuarioPorEmail(String email) {
-        return usuarios.get(email);
+        return usuariosPorEmail.get(email);
     }
 
-    public Map<String, Usuario> obtenerUsuarios() {
-        return usuarios;
+    public Map<String, Usuario> obtenerUsuariosPorEmail() {
+        return usuariosPorEmail;
+    }
+
+    public Usuario obtenerUsuarioPorId(int id) throws UsuarioNoEncontradoException {
+        return usuarios.stream()
+                .filter(u -> u.getId() == id)
+                .findFirst()
+                .orElseThrow(() -> new UsuarioNoEncontradoException("Usuario con ID " + id + " no encontrado."));
     }
 }
+
 
